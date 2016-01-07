72e61f1891716a827060dc8a0731b1f20ecc07f3
roma0104 <roma0104@gmail.com>
2016-01-06 19:27:48 -0600

    Added more documentation.

--- bin/build.sh
+++ bin/build.sh
@@ -7,6 +7,8 @@ OUTPUT_DIR=$GSD5DOCS_DIR/output
 OUTPUT_FILE=gsd5-docs.html
 mkdir -p $OUTPUT_DIR
 
+export TIDDLYWIKI_PLUGIN_PATH=$GSD5DOCS_DIR/plugins
+
 ( cd $TIDDLYWIKI5_DIR && node ./tiddlywiki.js \
 	$GSD5DOCS_DIR/editions/gsd5-docs \
 	--verbose \
